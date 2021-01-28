62f000bc7f9ec5cdb176fa56f7202c430ab27325
Pavel Vasev <pavel.vasev@gmail.com>
2021-01-28 14:18:38 +0500

    fixes

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-// Enter your app code here
+/* Hello!
 
-console.log("hello Viewzavr!");
+This file is loaded by index.html, you may enter your app code here.
+This method is just for example and all is up to you.
 
-/*
-example codes:
+Example codes:
 
 var pts = vz.vis.addPoints();
 pts.positions = [1,2,3, 1,2,5, 1,3,12];
 
 var lins = vz.vis.addLines();
 lins.positions = [1,2,3, 1,2,5, 1,2,5, 1,3,12];
+
+vz.restoreFromHash(); // this restores app state from url (e.g. all parameters and manually added items)
+
 */
 
+console.log("hello Viewzavr!");
