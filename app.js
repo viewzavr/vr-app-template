7c7ceddac9aed1b47f85c3ea8721e2dcfba644fd
Pavel Vasev <pavel.vasev@gmail.com>
2021-01-29 14:17:40 +0500

    words

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 console.log("hello Viewzavr!");
 
+// var obj = vz.vis.addPoints();
+
 
 // this restores app state from url (e.g. all parameters and items added in visual interface)
 vz.restoreFromHash();
